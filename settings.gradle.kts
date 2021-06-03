8de8604b0d31206f302232c27c1c0053a4f876e1
Roberto Casadei <robyonrails@gmail.com>
2021-06-03 16:36:37 +0200

    OOP machinery

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,22 @@
-rootProject.name = "covid-sim"
+import org.danilopianini.VersionAliases.justAdditionalAliases
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.danilopianini:refreshversions-aliases:+")
+    }
+}
+
+plugins {
+    id("de.fayard.refreshVersions") version "0.10.0"
+}
+
+refreshVersions {
+    extraArtifactVersionKeyRules = justAdditionalAliases
+}
+
+
+rootProject.name = "oop-machinery-covid-sim"
 include("test")
